fe1c01d6a24fa910586be387dc8f4924648a3ff3
M-Dimon <83703270+M-Dimon@users.noreply.github.com>
2024-08-29 09:27:59 +0200

    Focus view & Routing
    Focus view created for some themes
    
    Routing for new pages created

---
+++ Styling-Layout-GoERP/FocusViewLightTheme.xaml.cs
@@ -0,0 +1,9 @@
+namespace Styling_Layout_GoERP;
+
+public partial class FocusViewLightTheme : ContentPage
+{
+	public FocusViewLightTheme()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Styling-Layout-GoERP/FocusViewStandardTheme.xaml.cs
@@ -0,0 +1,9 @@
+namespace Styling_Layout_GoERP;
+
+public partial class FocusViewStandardTheme : ContentPage
+{
+	public FocusViewStandardTheme()
+	{
+		InitializeComponent();
+	}
+}
