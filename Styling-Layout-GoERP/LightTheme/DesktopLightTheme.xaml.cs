419db7e3674fd576f94204d5d16bc6487629e11a
M-Dimon <83703270+M-Dimon@users.noreply.github.com>
2024-08-30 12:39:02 +0200

    Desktop frontpage
    Desktop Front page for Standard and Light Theme

---
+++ Styling-Layout-GoERP/LightTheme/DesktopLightTheme.xaml.cs
@@ -0,0 +1,9 @@
+namespace Styling_Layout_GoERP.LightTheme;
+
+public partial class DesktopLightTheme : ContentPage
+{
+	public DesktopLightTheme()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Styling-Layout-GoERP/StandardTheme/DesktopStandardTheme.xaml.cs
@@ -0,0 +1,9 @@
+namespace Styling_Layout_GoERP.StandardTheme;
+
+public partial class DesktopStandardTheme : ContentPage
+{
+	public DesktopStandardTheme()
+	{
+		InitializeComponent();
+	}
+}
